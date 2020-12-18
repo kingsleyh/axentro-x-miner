80874a7ee1acded694ef92b4d5002b9b0149aef1
Kingsley <kingsleyhendrickse@me.com>
2020-12-18 00:10:14 +0000

    argon2 working

--- argon2/argon2.v
+++ argon2/argon2.v
@@ -1,9 +1,13 @@
 module argon2
 
-#flag -I @VROOT/c
-#flag @VROOT/c
+#flag @VROOT/c/src/argon2.o
+#flag @VROOT/c/src/core.o
+#flag @VROOT/c/src/thread.o
+#flag @VROOT/c/src/encoding.o
+#flag @VROOT/c/src/blake2/blake2b.o
+#flag @VROOT/c/src/ref.o
 
-#include "include/argon2.h"
-
+#flag -I @VROOT/c/include
+#include "argon2.h"
 
 fn C.argon2id_hash_raw(t_cost u32, m_cost u32, parallelism u32, pwd voidptr, pwdlen u32, salt voidptr, saltlen u32, hash voidptr, hashlen u32) int
